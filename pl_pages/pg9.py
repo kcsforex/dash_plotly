7ec1564c4ee3d0d28701170d500ea3ae49007d4a
kcsforex <110934502+kcsforex@users.noreply.github.com>
2023-11-29 10:26:31 +0100

    Add files via upload

--- pl_pages/pg9.py
+++ pl_pages/pg9.py
@@ -1,39 +1,26 @@
-# 2023.11.28  19.00
+# 2023.11.29  10.00
 import pandas as pd
-from binance.client import Client
 import pandas_ta as ta
-import config
 import time
 import dash
 from dash import html, dcc, callback, Output, Input
 import dash_bootstrap_components as dbc
+from mexc_crypto_api import crypto_candles_df, act_price
 
-dash.register_page(__name__, name='Binance Crypto Charts')
+dash.register_page(__name__, name='MEXC Crypto Info')
 
-client = Client(config.API_KEY, config.API_SECRET)
-def GetKlines(CryptoSymbol):
-
-    klines = client.get_klines(symbol=CryptoSymbol, interval=Client.KLINE_INTERVAL_1MINUTE, limit='90')
-    klines_full_df = pd.DataFrame(klines, columns = ['date', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quate_asset_volume', 'no_trades', 'base_asset_volume', 'quote_asset_volume', 'ignore'])
-    klines_df = klines_full_df.loc[:,['date', 'open', 'high', 'low', 'close', 'volume']]
-    klines_df['date'] = pd.to_datetime(klines_df['date'], unit='ms')
-    for col in ['open', 'high', 'low', 'close','volume']: 
-        klines_df[col] = pd.to_numeric(klines_df[col],errors='coerce')
-    return klines_df
-
-MyCryptoNames = ['BTCUSDT','ETHUSDT','COMPUSDT','FILUSDT','MKRUSDT','BCHUSDT','SOLUSDT','AVAXUSDT','SANDUSDT','MANAUSDT']
-#'ETCUSDT','DOGEUSDT','AXSUSDT','APTUSDT','MASKUSDT','MAGICUSDT','OPUSDT','XRPUSDT','BNBUSDT','LTCUSDT']
-
+MyCryptoNames = ['AVAXUSDT','AXSUSDT','BCHUSDT','BTCUSDT','COMPUSDT','DOGEUSDT','ETHUSDT','FILUSDT','LUNCUSDT','LUNAUSDT','MANAUSDT','MKRUSDT','PEPEUSDT','SOLUSDT','SANDUSDT']
+#'ETCUSDT','APTUSDT','MASKUSDT','MAGICUSDT','OPUSDT','XRPUSDT','BNBUSDT','LTCUSDT']
 
 layout = dbc.Container([
 
  dbc.Row([
-    html.Div('Streaming Crypto Charts (Updated every 30sec)', className="text-primary text-center fs-4")
+    html.Div('Streaming Crypto Table-Charts (Refresh)', className="text-primary text-center fs-4")
      ]),
     
     dbc.Row([ 
         dbc.Card( 
-            html.Div(id='crypto-table', style={  'margin':'15px',"maxHeight": "400px", "overflow": "auto"}),
+            html.Div(id='crypto-table', style={  'margin':'15px',"maxHeight": "550px", "overflow": "auto"}),
             style= { 'box-shadow': '5px 5px 5px gray', 'borderRadius':'10px',"font-size": "12px"}), #overflow=Auto           
         dcc.Interval(id='refresh-interval', interval=30000, n_intervals=0),
     ]),
@@ -47,7 +34,24 @@ def update_crypto_table(n_intervals):
 
     for crypto in MyCryptoNames:   
 
-        klines_df = GetKlines(crypto) 
+        klines_df = crypto_candles_df(crypto,1,100)
+
+        if crypto == 'LUNCUSDT':
+            klines_df['open'] = 1000 * klines_df['open']  
+            klines_df['high'] = 1000 * klines_df['high'] 
+            klines_df['low']  = 1000 * klines_df['low'] 
+            klines_df['close']= 1000 * klines_df['close']
+        elif crypto == 'PEPEUSDT':
+            klines_df['open'] = 1000000 * klines_df['open']  
+            klines_df['high'] = 1000000 * klines_df['high'] 
+            klines_df['low']  = 1000000 * klines_df['low'] 
+            klines_df['close']= 1000000 * klines_df['close']
+        else:
+            klines_df['open'] = klines_df['open']  
+            klines_df['high'] = klines_df['high'] 
+            klines_df['low']  = klines_df['low'] 
+            klines_df['close']= klines_df['close']
+
         M = 25
         # ---------- Smoothed H.Ashi ----------
         klines_df['o_ema'] = ta.ema(klines_df['open'],  length=M )
@@ -69,8 +73,17 @@ def update_crypto_table(n_intervals):
         high_1 = klines_df["high"].iloc[-2]
         low_0 = klines_df["low"].iloc[-1]
         low_1 = klines_df["low"].iloc[-2]
-        actual_price = round(float(client.get_symbol_ticker(symbol = crypto)['price']),3)
+
+        if crypto == 'LUNCUSDT':
+            actual_price = round(1000 * act_price(crypto),3)
+            crypto = '1000LUNCUSDT'
+        elif crypto == 'PEPEUSDT':
+            actual_price = round(1000000 * act_price(crypto),3)
+            crypto = '1Million_PEPEUSDT'
+        else:
+            actual_price = act_price(crypto)
         
+
         if (actual_price < sha_min and (high_0 or high_1) >= sha_min):
             text = 'SELL position!'
             #send_to_telegram(Crypto + ' sell position! Act.price: '+ str(actual_price))
@@ -82,7 +95,7 @@ def update_crypto_table(n_intervals):
             #order_succeeded = Crypto_Order('SIDE_SELL', 0.1, 'ETHUSDT', 'order_market_buy')
         else:
             text = 'Nothing to do'
-
+        
         
         crypto_info.append({'Name':crypto, 'Action:':text, 'Price':actual_price, 'SHA_High':sha_max,'SHA_Low':sha_min})
         time.sleep(0.1)
